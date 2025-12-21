a7a28dc4d99f60ee2491e55db4f915218ca682de
Binh Tran <binhtd.dev@gmail.com>
2025-12-22 00:04:02 +0700

    feat(keymaps): add keymap for delete other buffers except pinned

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,18 @@
 vim.keymap.set("n", "<leader>y", '"+yy', { desc = "[Y]ank to clipboard" })
 vim.keymap.set({ "v" }, "<leader>y", '"+y', { desc = "[Y]ank to clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "[P]aste from clipboard" })
+
+vim.keymap.set("n", "<leader>bo", function()
+  local bufferline = require("bufferline")
+  local commands = require("bufferline.commands")
+  local current = vim.api.nvim_get_current_buf()
+  vim.schedule(function()
+    for _, e in ipairs(bufferline.get_elements().elements) do
+      local is_current = e.id == current
+      local is_pinned = bufferline.groups._is_pinned(e)
+      if not is_current and not is_pinned then
+        commands.unpin_and_close(e.id)
+      end
+    end
+  end)
+end, { desc = "Delete Other Buffers (except pinned)" })
