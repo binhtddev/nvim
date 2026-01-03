552d1b1ed9d400492c1f8b54bf19be6fe7fe41f9
Binh Tran <binhtd.dev@gmail.com>
2026-01-03 18:15:09 +0700

    feat(editor/flash.nvim): add expand/shrink selection

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,2 +1,29 @@
+local function shrink_expand_selection()
+  require("flash").treesitter({
+    actions = {
+      ["<a-o>"] = "next",
+      ["<a-i>"] = "prev",
+    },
+    labels = "",
+  })
+end
+
 return {
+  {
+    "folke/flash.nvim",
+    keys = {
+      {
+        "<a-o>",
+        mode = { "n", "o", "x" },
+        shrink_expand_selection,
+        desc = "Treesitter Expand Selection",
+      },
+      {
+        "<a-i>",
+        mode = { "n", "o", "x" },
+        shrink_expand_selection,
+        desc = "Treesitter Shrink Selection",
+      },
+    },
+  },
 }
