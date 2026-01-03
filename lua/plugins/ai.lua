20261204daf35091d2c5dcff33b23c47b90174eb
Binh Tran <binhtd.dev@gmail.com>
2026-01-03 19:37:28 +0700

    fix(ai): lazy keybindings

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -5,26 +5,32 @@ return {
     config = function()
       local neocodeium = require("neocodeium")
       neocodeium.setup({
-        enabled = false
+        enabled = false,
       })
-      vim.keymap.set("i", "<A-space>", function()
-        require("neocodeium").accept()
-      end)
-      vim.keymap.set("i", "<A-s-space>", function()
-        require("neocodeium").accept_line()
-      end)
-      vim.keymap.set("i", "<A-w>", function()
-        require("neocodeium").accept_word()
-      end)
-      vim.keymap.set("i", "<A-e>", function()
-        require("neocodeium").cycle_or_complete()
-      end)
-      vim.keymap.set("i", "<A-r>", function()
-        require("neocodeium").cycle_or_complete(-1)
-      end)
-      vim.keymap.set("i", "<A-c>", function()
-        require("neocodeium").clear()
-      end)
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "NeoCodeiumServerConnected",
+        group = vim.api.nvim_create_augroup("user_neocodeium", { clear = true }),
+        callback = function()
+          vim.keymap.set("i", "<a-space>", function()
+            require("neocodeium").accept()
+          end)
+          vim.keymap.set("i", "<a-s-space>", function()
+            require("neocodeium").accept_line()
+          end)
+          vim.keymap.set("i", "<a-w>", function()
+            require("neocodeium").accept_word()
+          end)
+          vim.keymap.set("i", "<a-e>", function()
+            require("neocodeium").cycle_or_complete()
+          end)
+          vim.keymap.set("i", "<a-r>", function()
+            require("neocodeium").cycle_or_complete(-1)
+          end)
+          vim.keymap.set("i", "<a-c>", function()
+            require("neocodeium").clear()
+          end)
+        end,
+      })
     end,
   },
 }
